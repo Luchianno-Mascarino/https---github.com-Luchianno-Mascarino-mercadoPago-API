3174cb671227c265dde3922ebd9604ba2a304d37
Luchianno Mascarino <luchomascarino@gmail.com>
2023-10-30 17:25:12 -0300

    ADD morgan index.js ADD new file config.js con las configuraciones del servidor

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import morgan from 'morgan'
 import paymentRoutes from '../src/routes/payment.routes.js';
+import {PORT} from '../src/routes/config.js';
 
 
-const app = express()
+const app = express();
+
+app.use(morgan('dev'));
 
 app.use(paymentRoutes);
 
-app.listen(3000)
-console.log('Server on port', 3000)
+app.listen(PORT);
+console.log('Server on port', PORT);
